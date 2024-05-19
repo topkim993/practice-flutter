f54f0eb34b351620a044b10865e8dbc9a28c78cc
Jeongsang Kim <topkim.dev@gmail.com>
2024-05-19 14:26:25 +0900

    feat: Create Flutter Project

---
+++ toonflix/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  List<int> numbers = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Large Title",
+                style: TextStyle(fontSize: 30),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
