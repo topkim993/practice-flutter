b0aae2dd390ed30eb67cbcea2cbae175fb5f4a53
Jeongsang Kim <topkim.dev@gmail.com>
2024-07-13 15:43:01 +0900

    feat: Home 구성

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,85 +1,22 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
+import 'home_page.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  bool showTitle = true;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Wedding Invitation',
       theme: ThemeData(
-        textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            fontSize: 30,
-            color: Colors.red,
-          ),
-        ),
+        primarySwatch: Colors.pink,
       ),
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              showTitle ? const MyLargeTitle() : const Text("not showing"),
-              IconButton(
-                onPressed: () {
-                  setState(() {
-                    showTitle = !showTitle;
-                  });
-                },
-                icon: const Icon(Icons.remove_red_eye),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  @override
-  void initState() {
-    super.initState();
-    print("initState!");
-  }
-
-  @override
-  void dispose() {
-    print("dispose");
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print("build!");
-    return Text(
-      "Large Title",
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge?.color ?? Colors.black,
-      ),
+      home: const HomePage(),
     );
   }
 }
