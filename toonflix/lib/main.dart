cd5488211dbab3ab27943e426fb948e6ee0a1fdf
Jeongsang Kim <topkim.dev@gmail.com>
2024-07-13 10:04:05 +0900

    feat: 샘플 main dart 추가

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const App());
@@ -12,23 +14,71 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  List<int> numbers = [];
+  bool showTitle = true;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            fontSize: 30,
+            color: Colors.red,
+          ),
+        ),
+      ),
       home: Scaffold(
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                "Large Title",
-                style: TextStyle(fontSize: 30),
-              ),
+              showTitle ? const MyLargeTitle() : const Text("not showing"),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    showTitle = !showTitle;
+                  });
+                },
+                icon: const Icon(Icons.remove_red_eye),
+              )
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatefulWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  @override
+  void initState() {
+    super.initState();
+    print("initState!");
+  }
+
+  @override
+  void dispose() {
+    print("dispose");
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("build!");
+    return Text(
+      "Large Title",
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color ?? Colors.black,
       ),
     );
   }
