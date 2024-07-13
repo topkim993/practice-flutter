140bca2ca9aec93a8ad5c3ef5fe7f4414a819bcf
Jeongsang Kim <topkim.dev@gmail.com>
2024-07-13 15:41:25 +0900

    feat: Section Container 추가

---
+++ toonflix/lib/sections/section_container.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Section extends StatelessWidget {
+  final String title;
+  final Widget child;
+
+  const Section({
+    super.key,
+    required this.title,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 8),
+          child,
+        ],
+      ),
+    );
+  }
+}
